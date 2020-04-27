11ec4ebaee0b02c01b61fe9b2dc7a2f5ebb949b1
Andrey Prjibelski <andrewprzh@gmail.com>
2020-04-28 00:14:50 +0300

    fix 1 base exons

--- src/common.py
+++ src/common.py
@@ -48,7 +48,7 @@ def overlaps(range1, range2):
 
 
 def overlaps_at_least(range1, range2, delta = 0):
-    cutoff = min([delta, range1[1] - range1[0], range2[1] - range2[0]])
+    cutoff = min([delta, range1[1] - range1[0] + 1, range2[1] - range2[0] + 1])
     overlap = min(range1[1], range2[1]) + 1 - max(range1[0], range2[0])
     return overlap >= cutoff
 
